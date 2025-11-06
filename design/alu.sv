444e9020b61e3722b938a5112a2cf75514130af5
Miriam Gonzaga <miriam.gonzaga@ufpe.br>
2025-11-06 11:57:10 -0300

    operações alu -100%

--- design/alu.sv
+++ design/alu.sv
@@ -17,8 +17,16 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:        // SUB
+                    ALUResult = SrcA - SrcB;
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+            4'b0011:        // OR
+                    ALUResult = SrcA | SrcB;
+            4'b0100:        //XOR
+                    ALUResult = SrcA ^ SrcB;
+            4'b0101:        //SLT
+                    ALUResult = (SrcA < SrcB) ? 1 : 0;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
             default:
