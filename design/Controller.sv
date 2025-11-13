c67bb0f3468aaafbdcb2f5e0a156fc67b61a766a
Miriam Gonzaga <miriam.gonzaga@ufpe.br>
2025-11-13 12:51:36 -0300

    controller atualizado com imms

--- design/Controller.sv
+++ design/Controller.sv
@@ -15,7 +15,7 @@ module Controller (
     output logic RegWrite, //The register on the Write register input is written with the value on the Write data input 
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
-    output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
+    output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype/IMM
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
@@ -25,13 +25,14 @@ module Controller (
   assign LW = 7'b0000011;  //lw
   assign SW = 7'b0100011;  //sw
   assign BR = 7'b1100011;  //beq
+  assign IMM = 7'b0010011;  //imm
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == IMM);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == IMM);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR);
-  assign ALUOp[1] = (Opcode == R_TYPE);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == IMM);
   assign Branch = (Opcode == BR);
 endmodule
