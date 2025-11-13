6258a17bf7a92407fe174fbb1b9d2d420ebfefeb
Marioivsr <mdtsf@cin.ufpe.br>
2025-11-13 06:23:27 -0300

    Update ALUController.sv
    Mudei uma coisa no add pra ver algo

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -18,7 +18,7 @@ module ALUController (
 
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0000000)) ||  // R\I-add
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) ||  // R\I-add
       ((ALUOp == 2'b10) && (Funct3 == 3'b110)) || //or
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
 
